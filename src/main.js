571ea9c442d5957972570c524999f5512dd39626
toby <tobytb@163.com>
2018-06-20 11:06:17 +0800

    修复登录关闭浏览器后重新打开上次登录用户的任意页面，获取数据http状态为401而无法登录认证

--- src/main.js
+++ src/main.js
@@ -7,10 +7,25 @@ import iView from 'iview'
 import 'iview/dist/styles/iview.css'
 import Vuex from 'vuex'
 import store from './store'
+import axios from 'axios'
+import { removeLoginUserFromStorage } from '@/util/storage'
 Vue.use(Vuex)
 Vue.use(iView)
 Vue.config.productionTip = false
 
+axios.interceptors.response.use(function (response) {
+    // Do something with response data
+    return response
+}, function (error) {
+    // Do something with response error
+    if (error.response.status === 401) {
+        // 没有授权，重新登录
+        // 清除本地登录信息
+        removeLoginUserFromStorage()
+        router.push({ name: 'login' })
+    }
+    return Promise.reject(error)
+})
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
